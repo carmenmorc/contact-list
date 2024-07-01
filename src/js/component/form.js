e26cef7aa3419a6c34347f8b1db894692c94a198
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-07-01 20:24:32 +0000

    Form

---
+++ src/js/component/form.js
@@ -0,0 +1,35 @@
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
+
+export const Form = () => {
+
+    const {store, actions} = useContext(Context);
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        actions.addContact(formData);
+    };
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        phone: '',
+        address: ''
+    });
+
+    const handleChange = (e) => {
+        setFormData({...formData, [e.target.name] : e.target.value});
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit} className="w-50 mx-auto">
+                <input type="text" className="form-control" placeholder="Full Name" value={formData.name} name="name" onChange={handleChange}/>
+                <input type="text" className="form-control" placeholder="E-Mail" value={formData.email} name="email" onChange={handleChange}/>
+                <input type="text" className="form-control" placeholder="Phone" value={formData.phone} name="phone" onChange={handleChange}/>
+                <input type="text" className="form-control" placeholder="Address" value={formData.address} name="address" onChange={handleChange}/>
+                <input type="submit" className="btn btn-primary" value="Add Contact" />
+            </form>
+        </>
+    );
+};
